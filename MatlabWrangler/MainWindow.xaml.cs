9f6790e1a5b463c5e01df8af61946885994f455f
Scott Christensen <mscottchristensen@gmail.com>
2016-01-13 17:03:35 -0700

    I know I said I wouldn't do more, but I'm glad I did... I added multi-threading so that the UI can run unimpeded while the machine works away. This technically makes it so you can translate several simultaneously on different cores, but we won't worry about that for now. I have that case blocked for now

--- MatlabWrangler/MainWindow.xaml.cs
+++ MatlabWrangler/MainWindow.xaml.cs
@@ -24,43 +24,179 @@ namespace MatlabWrangler
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region variables
         //lists to keep track of
         private BindingList<string> models = new BindingList<string>();
         private BindingList<string> filepaths = new BindingList<string>();
+        BackgroundWorker numberCruncher;
+        private bool numberCrunchingInProgress;
+        #endregion
 
+        #region UI functions
         public MainWindow()
         {
             InitializeComponent();
+            
             //link up to those bindings
             modelBox.ItemsSource = models;
             outputBox.ItemsSource = filepaths;
+
+            //numberCrunchingInProgress needs to be initialized to false so we can use the UI, it will lock kind of while it is crunching
+            numberCrunchingInProgress = false;
         }
-       
+
         private void generate_button_Click(object sender, RoutedEventArgs e)
         {
+            if (numberCrunchingInProgress == false)
+            {
+                //lock down the UI (kind of... it isn't frozen, but now just returns message boxes)
+                numberCrunchingInProgress = true;
+
+                //generating background process message
+                myTextBox.Text = "generating the background thread to crunch the numbers";
+
+                //initialize the background process
+                numberCruncher = new BackgroundWorker();
+                //reports the progress for our progress bar
+                numberCruncher.WorkerReportsProgress = true;
+                numberCruncher.DoWork += new DoWorkEventHandler(numberCruncher_DoWork);
+                numberCruncher.RunWorkerCompleted += new RunWorkerCompletedEventHandler(numberCruncher_RunWorkerCompleted);
+                numberCruncher.ProgressChanged += new ProgressChangedEventHandler(numberCruncher_ReportProgress);
+                
+                //starts the asyncronous computation, leaving your UI free to be operated on
+                numberCruncher.RunWorkerAsync();
+            }
+            else
+            {
+                MessageBox.Show("You cannot start the generation process while it is already running! Patience, young padawan...");
+            }
+        }
+        
+        private void choose_folder_click(object sender, RoutedEventArgs e)
+        {
+            if (numberCrunchingInProgress == false)
+            {
+                CommonOpenFileDialog openFileDialog = new CommonOpenFileDialog();
+                openFileDialog.Title = "Destination Folder for .m file";
+                openFileDialog.IsFolderPicker = true;
+                //openFileDialog.InitialDirectory = ???
+
+                openFileDialog.AddToMostRecentlyUsedList = false;
+                openFileDialog.AllowNonFileSystemItems = false;
+                //openFileDialog.DefaultDirectory = ???
+                openFileDialog.EnsureFileExists = true;
+                openFileDialog.EnsurePathExists = true;
+                openFileDialog.EnsureReadOnly = false;
+                openFileDialog.EnsureValidNames = true;
+                openFileDialog.Multiselect = false;
+                openFileDialog.ShowPlacesList = true;
+
+                //did it work?
+                if (openFileDialog.ShowDialog() == CommonFileDialogResult.Ok)
+                {
+                    string folder = openFileDialog.FileName;
+                    //refresh folder list...
+                    string filePath = folder + "\\defineVehicleBody.m";
+                    filepaths.Add(filePath);
+                    outputBox.ItemsSource = null;
+                    outputBox.ItemsSource = filepaths;
+
+                    //now tell the user to push generate
+                    myTextBox.Text = "after dragging a .stl file into the top box, push generate";
+                }
+            }
+            else
+            {
+                MessageBox.Show("You can't swap folders while the .m file is being generated. Patience, young padawan...");
+            }
+        }
+
+        private void modelBox_DragEnter(object sender, DragEventArgs e)
+        {
+            if (numberCrunchingInProgress == false)
+            {
+                if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                    e.Effects = DragDropEffects.Copy;
+            }
+            else
+            {
+                MessageBox.Show("Stop doing stuff while it is working!");
+            }
+        }
+
+        private void modelBox_Drop(object sender, DragEventArgs e)
+        {
+            if (numberCrunchingInProgress == false)
+            {
+                string[] modelPaths = (string[])e.Data.GetData(DataFormats.FileDrop);
+                foreach (string modelPath in modelPaths)
+                {
+                    //make sure that the path is a .stl path
+                    string extension = modelPath.Split('.').Last();
+                    if (extension.Equals("stl"))
+                    {
+                        if (models.Count > 0)
+                            models.Clear();
+                        models.Add(modelPath);
+                    }
+                    else
+                        MessageBox.Show("Only .stl files are allowed!");
+                }
+                //refresh listbox
+                modelBox.ItemsSource = null;
+                modelBox.ItemsSource = models;
+            }
+            else
+            {
+                MessageBox.Show("Stop doing stuff while it is working!");
+            }
+        }
+        #endregion
+
+        #region background processes
+        private void numberCruncher_DoWork(object sender, DoWorkEventArgs e)
+        {
+            numberCruncher.ReportProgress(0, "Opening Files...");
+
             //open files to operate on (the .m file and the .stl)
             TextWriter mFile = new StreamWriter(filepaths.FirstOrDefault());
             StreamReader stlFile = new StreamReader(models.FirstOrDefault());
             List<List<double>> vertexList = new List<List<double>>();
             List<List<int>> faceList = new List<List<int>>();
             int vertexIndex = 0;
-            
-            if(ASCII.IsChecked == true)
-            {
-                stlFile.ReadLine();                                         //solid Default
-                while(stlFile.ReadLine().Split(' ').First() != "endsolid")  //  facet normal
+
+            //could use this if need be in the future for processing binary files too, but in the end you could just check for binary files and this ASCII object is owned by another thread
+            //if (ASCII.IsChecked == true)
+            //{
+                numberCruncher.ReportProgress(0, "Counting lines in .stl file...");
+                double lines = 0;
+                double loops = 0;
+                using (StreamReader counter = new StreamReader(models.FirstOrDefault()))
                 {
+                    while (counter.Peek() >= 0)
+                    {
+                        lines++;
+                        counter.ReadLine();
+                    }
+                    counter.Close();
+                    loops = (lines - 2) / 7; 
+                }
+
+                double currentLoop = 0;
+                numberCruncher.ReportProgress(5,string.Format("Converting and sorting .stl file... current loop: {0} of {1}", currentLoop, loops));
+
+
+                stlFile = new StreamReader(models.FirstOrDefault());
+                stlFile.ReadLine();                                         //solid Default
+                while (stlFile.ReadLine().Split(' ').First() != "endsolid")  //  facet normal
+                {
+                    currentLoop++;
                     stlFile.ReadLine();                                     //    outer loop
                     string[] vertex1 = stlFile.ReadLine().TrimStart(' ').Split(' '); //vertex
                     string[] vertex2 = stlFile.ReadLine().TrimStart(' ').Split(' '); //vertex
                     string[] vertex3 = stlFile.ReadLine().TrimStart(' ').Split(' '); //vertex
                     List<double> tempVertex = new List<double>();
                     List<int> tempFace = new List<int>();
-
-                    //invert the z coordinate so that positive is down
-                    vertex1[3] = (-Convert.ToDouble(vertex1[3])).ToString();
-                    vertex2[3] = (-Convert.ToDouble(vertex2[3])).ToString();
-                    vertex3[3] = (-Convert.ToDouble(vertex3[3])).ToString();
 
                     //first vertex
                     for (int i = 1; i < 4; i++)
@@ -73,7 +209,7 @@ namespace MatlabWrangler
                     }
                     tempFace.Add(vertexIndex);
                     tempVertex = new List<double>();    //make a new set of doubles to record
-                    //second vertex
+                                                        //second vertex
                     for (int i = 1; i < 4; i++)
                         tempVertex.Add(Convert.ToDouble(vertex2[i]));
                     vertexIndex = indexFound(vertexList, tempVertex);
@@ -84,7 +220,7 @@ namespace MatlabWrangler
                     }
                     tempFace.Add(vertexIndex);
                     tempVertex = new List<double>();    //make a new set of doubles to record
-                    //third vertex
+                                                        //third vertex
                     for (int i = 1; i < 4; i++)
                         tempVertex.Add(Convert.ToDouble(vertex3[i]));
                     vertexIndex = indexFound(vertexList, tempVertex);
@@ -95,15 +231,16 @@ namespace MatlabWrangler
                     }
                     tempFace.Add(vertexIndex);
                     tempVertex = new List<double>();    //make a new set of doubles to record
-                    //vertecies added to triangle, add the triangle to the triangle list
+                                                        //vertecies added to triangle, add the triangle to the triangle list
                     faceList.Add(tempFace);
                     tempFace = new List<int>(); //new triangle without clearing old one
-                    //proceed to the next set
+                                                //proceed to the next set
                     stlFile.ReadLine(); //endloop
                     stlFile.ReadLine(); //endfacet
+                    numberCruncher.ReportProgress((int)((currentLoop/loops)*90) + 5, string.Format("Converting and sorting .stl file... current loop: {0} of {1}", currentLoop, loops)); //these loops will acount for ~90% of the work I think
                 }
                 stlFile.Close(); //we're done with this file, it is now in memory
-            }
+            //}
 
             double maxX = 0.0;
             double minX = 0.0;
@@ -111,12 +248,21 @@ namespace MatlabWrangler
             double minY = 0.0;
             double maxZ = 0.0;
             double minZ = 0.0;
-            foreach(List<double> vertex in vertexList)
+            foreach (List<double> vertex in vertexList)
             {
-                vertex[0] /= 8;
-                vertex[1] /= 8;
-                vertex[2] /= 8;
-                if(vertex[0] > maxX)
+                //translate
+                vertex[0] += 12;        //x shift before flip
+                vertex[2] -= 10;        //z shift before flip
+
+                //flip
+                vertex[0] = -vertex[0];   //x is flipped so that positive is forward in the plane frame
+                vertex[2] = -vertex[2];   //z is flipped so that positive is down in the plane frame
+
+                //scale
+                vertex[0] /= 6.418734;
+                vertex[1] /= 6.418734;
+                vertex[2] /= 6.418734;
+                if (vertex[0] > maxX)
                 {
                     maxX = vertex[1];
                 }
@@ -141,6 +287,7 @@ namespace MatlabWrangler
                     minZ = vertex[1];
                 }
             }
+            numberCruncher.ReportProgress(96, "Writing Verticies \"V\" Matrix...");
 
             //now we are ready to write the .m file!
             //write the first lines of the .m file
@@ -169,6 +316,7 @@ namespace MatlabWrangler
             }
             mFile.WriteLine("    ]';");
             mFile.WriteLine();
+            numberCruncher.ReportProgress(97, "Writing Faces \"F\" Matrix...");
 
             //define faces
             mFile.WriteLine("% Define Faces");
@@ -179,6 +327,7 @@ namespace MatlabWrangler
             }
             mFile.WriteLine("    ];");
             mFile.WriteLine();
+            numberCruncher.ReportProgress(98, "Writing Colors Matrix...");
 
             //define color choices
             mFile.WriteLine("myred = [1, 0, 0];");
@@ -186,89 +335,47 @@ namespace MatlabWrangler
             mFile.WriteLine("mygreen = [0, 1, 0];");
             mFile.WriteLine("mycyan = [0, 1, 1];");
             mFile.WriteLine("myblue = [0, 0, 1];");
+            mFile.WriteLine("mygrey = [0.5, 0.5, 0.5];");
             mFile.WriteLine();
 
             mFile.WriteLine("% Define Faces");
             mFile.WriteLine("facecolors = [...");
             for (int i = 0; i < faceList.Count; i++)
             {
-                mFile.WriteLine("    mygreen;...");
+                mFile.WriteLine("    mygrey;...");
             }
             mFile.WriteLine("    ];");
+            numberCruncher.ReportProgress(99, "Closing Files...");
 
             //finish up and close the files
             mFile.WriteLine("end");
             mFile.Close();
             stlFile.Close();
+        }
+
+        private void numberCruncher_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
             MessageBox.Show("File has been generated!");
+            myProgressBar.Value = 100;
+            myTextBox.Text = "File Generation Complete.";
+        }
+
+        private void numberCruncher_ReportProgress(object sender, ProgressChangedEventArgs e)
+        {
+            myProgressBar.Value = e.ProgressPercentage;
+            myTextBox.Text = (string)(e.UserState);
         }
 
         private int indexFound(List<List<double>> listOfVerticies, List<double> vertex)
         {
             int i = 0;
-            foreach (List<double> listVertex in listOfVerticies)
+            if (!listOfVerticies.Contains(vertex))
+                return -1;
+            else
             {
-                if (vertex.SequenceEqual(listVertex))
-                    return i;
-                i++;
-            }
-            return -1;
-        }
-
-        private void choose_folder_click(object sender, RoutedEventArgs e)
-        {
-            CommonOpenFileDialog openFileDialog = new CommonOpenFileDialog();
-            openFileDialog.Title = "Destination Folder for .m file";
-            openFileDialog.IsFolderPicker = true;
-            //openFileDialog.InitialDirectory = ???
-
-            openFileDialog.AddToMostRecentlyUsedList = false;
-            openFileDialog.AllowNonFileSystemItems = false;
-            //openFileDialog.DefaultDirectory = ???
-            openFileDialog.EnsureFileExists = true;
-            openFileDialog.EnsurePathExists = true;
-            openFileDialog.EnsureReadOnly = false;
-            openFileDialog.EnsureValidNames = true;
-            openFileDialog.Multiselect = false;
-            openFileDialog.ShowPlacesList = true;
-            
-            //did it work?
-            if(openFileDialog.ShowDialog() == CommonFileDialogResult.Ok)
-            {
-                string folder = openFileDialog.FileName;
-                //refresh folder list...
-                string filePath = folder + "\\defineVehicleBody.m";
-                filepaths.Add(filePath);
-                outputBox.ItemsSource = null;
-                outputBox.ItemsSource = filepaths;
+                return listOfVerticies.IndexOf(vertex);
             }
         }
-
-        private void modelBox_DragEnter(object sender, DragEventArgs e)
-        {
-            if (e.Data.GetDataPresent(DataFormats.FileDrop))
-                e.Effects = DragDropEffects.Copy;
-        }
-
-        private void modelBox_Drop(object sender, DragEventArgs e)
-        {
-            string[] modelPaths = (string[])e.Data.GetData(DataFormats.FileDrop);
-            foreach (string modelPath in modelPaths)
-            {
-                //make sure that the path is a .stl path
-                string extension = modelPath.Split('.').Last();
-                if (extension.Equals("stl"))
-                {
-                    if (models.Count > 0)
-                        models.Clear();
-                    models.Add(modelPath);
-                }
-                else
-                    MessageBox.Show("Only .stl files are allowed!");
-            }
-            //refresh listbox
-            modelBox.ItemsSource = null;
-            modelBox.ItemsSource = models;
-        }
+        #endregion
     }
 }
